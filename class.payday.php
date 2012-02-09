8c35947f39e223826fa2de10321cff84bab364dd
Gert Van de Ven <gert@kww.be>
2012-02-09 15:22:12 +0100

    adding functions

--- class.payday.php
+++ class.payday.php
@@ -1,1 +1,27 @@
 <?php
+
+class payday {
+	
+	var $usera = "usera";
+	var $userb = "userb";
+	
+	var $transactions = array();
+	
+	function set_names($names) {
+		$this->usera = $names['usera'];
+		$this->userb = $names['userb'];
+	}
+	
+	function get_last_payment() {
+		
+	}
+	
+	function get_balance() {
+		
+	}
+	
+	function get_transactions() {
+		$this->transactions = array("blabla");
+	}
+	
+}
