3f5b11b70f2daf78c3ff862941f3c759b5af9074
Gert Van de Ven <gert@kww.be>
2012-02-09 15:47:50 +0100

    Added header

---
+++ payday_remote/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+# configuration file
+
+# database information
+$db_info['host'] = "localhost";
+$db_info['username'] = "vandeven_gert";
+$db_info['password'] = "292580";
+$db_info['database'] = "vandeven_db";
+
+$user_info[] = array("username" => "user1", "password" => "password1");
+$user_info[] = array("username" => "user2", "password" => "password2");
+
+?>
